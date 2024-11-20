120a77af3f46bfc5620045a697341a13854ebc61
gabrielpellizzon <gabrielpellizzon@gmail.com>
2024-11-19 21:37:46 -0300

    feat - CRUD for Client model

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { ClientModule } from './client/client.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, ClientModule],
   controllers: [AppController],
   providers: [AppService],
 })
